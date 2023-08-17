8c48d25af9ff80e341d1500f99f3d262049bfa8e
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2023-08-17 12:16:45 +0000

    format

--- src/pixparse/task/task_factory.py
+++ src/pixparse/task/task_factory.py
@@ -12,7 +12,16 @@ from pixparse.models import Cruller, ModelCfg, get_model_config
 from pixparse.tokenizers import TokenizerHF, TokenizerCfg
 from pixparse.data import preprocess_text_anno
 from pixparse.utils import get_ocr_metrics
-from pixparse.task import TaskCrullerEvalOCR, TaskCrullerEvalOCRCfg, TaskDonutEvalOCR, TaskDonutEvalOCRCfg, TaskCrullerPretrain, TaskCrullerPretrainCfg
+from pixparse.task import (
+    TaskCrullerEvalOCR,
+    TaskCrullerEvalOCRCfg,
+    TaskDonutEvalOCR,
+    TaskDonutEvalOCRCfg,
+    TaskCrullerPretrain,
+    TaskCrullerPretrainCfg,
+    TaskCrullerFinetune,
+    TaskCrullerFinetuneCfg
+)
 
 from chug.common import LoaderBundle
 
@@ -27,18 +36,25 @@ class TaskFactory:
     """
 
     TASK_CLASS_REGISTRY = {
-        'cruller_eval_ocr': (TaskCrullerEvalOCR, TaskCrullerEvalOCRCfg),
-        'donut_eval_ocr': (TaskDonutEvalOCR, TaskDonutEvalOCRCfg),
-        'cruller_pretrain': (TaskCrullerPretrain, TaskCrullerPretrainCfg)
+        "cruller_eval_ocr": (TaskCrullerEvalOCR, TaskCrullerEvalOCRCfg),
+        "donut_eval_ocr": (TaskDonutEvalOCR, TaskDonutEvalOCRCfg),
+        "cruller_pretrain": (TaskCrullerPretrain, TaskCrullerPretrainCfg),
+        "cruller_finetune": (TaskCrullerFinetune, TaskCrullerFinetuneCfg)
     }
 
     @classmethod
-    def create_task(cls, task_name: str, task_args, device_env: DeviceEnv, monitor: Monitor):
+    def create_task(
+        cls, task_name: str, task_args, device_env: DeviceEnv, monitor: Monitor
+    ):
         task_name = task_name.lower()
         if task_name not in cls.TASK_CLASS_REGISTRY:
-            raise ValueError(f"Unknown task type: {task_name}. Available tasks are {list(cls.TASK_CLASS_REGISTRY.keys())}")
+            raise ValueError(
+                f"Unknown task type: {task_name}. Available tasks are {list(cls.TASK_CLASS_REGISTRY.keys())}"
+            )
         task_cls = cls.TASK_CLASS_REGISTRY[task_name][0]
         task_cfg = cls.TASK_CLASS_REGISTRY[task_name][1]
         task_cfg_instance = task_cfg(**vars(task_args))
-        task_cls_instance = task_cls(cfg=task_cfg_instance, device_env=device_env, monitor=monitor)
+        task_cls_instance = task_cls(
+            cfg=task_cfg_instance, device_env=device_env, monitor=monitor
+        )
         return task_cls_instance, task_cfg_instance
