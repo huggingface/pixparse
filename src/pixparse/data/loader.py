f72c494dc4f55d8fe1b56db1a409d8b87abc781d
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2023-08-17 12:15:53 +0000

    allow hf datasets to be loaded here

--- src/pixparse/data/loader.py
+++ src/pixparse/data/loader.py
@@ -1,4 +1,5 @@
 from chug import create_wds_loader, create_doc_anno_pipe, create_image_text_pipe
+from chug.common import LoaderBundle, SharedCount
 
 from .config import DatasetCfg
 
@@ -7,15 +8,25 @@ from typing import Callable
 from datasets import load_dataset
 from torch.utils.data import DataLoader, DistributedSampler
 
-
+class GenericLoader(DataLoader):
+    """
+    supercharged dataloader for hf datasets to match methods from webdataset loaders. 
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.num_batches = len(self.dataset) // self.batch_size
+        if len(self.dataset) % self.batch_size != 0:
+            self.num_batches += 1
+        
 def create_loader(
     cfg: DatasetCfg,
     is_train: bool,
     image_preprocess,
     anno_preprocess,
-    dataset_format: str = "webdataset",
+    collate_fn: Callable = None,
     image_key="pdf;tif;tiff;png;jpg;jpeg",  # FIXME jpeg added for test w/ cc12m
     image_fmt="L",
+    start_interval: int = 0,
     seed: int = 0,
     world_size: int = 1,
     local_rank: int = 0,
@@ -27,9 +38,9 @@ def create_loader(
     Parameters:
         cfg (DatasetCfg): Configuration object for the dataset.
         is_train (bool): Indicates if the loader is for training data (True) or validation data (False).
+        collate_fn (Callable): Collate function to be used in loader. 
         image_preprocess (Callable): Image preprocessing sequence.
         anno_preprocess (Callable): Annotation preprocessing sequence.
-        dataset_format (str, optional): Format of the dataset, e.g., "webdataset" of "hf_dataset". Default is "webdataset".
         image_key (str, optional): Image formats/extensions that can be recognized and processed.
             Default includes formats such as "pdf", "tif", "tiff", etc.
         image_fmt (str, optional): Image format for reading images. Default is "L" (8-bit pixels, black and white).
@@ -64,14 +75,29 @@ def create_loader(
             world_size=world_size,
         )
     elif cfg.format == "hf_dataset":
-        dataset = load_dataset(cfg.source)
+        # In the case of hf datasets, we use the collator defined at task level
+        dataset = load_dataset(cfg.source)[cfg.split]
         training_sampler = DistributedSampler(
-            dataset, rank=local_rank, shuffle=True, seed=seed, num_replicas=world_size, 
+            dataset, rank=local_rank, shuffle=True, seed=seed, num_replicas=world_size, drop_last=True
         )
-        loader = DataLoader(
-            dataset,
-            batch_size=cfg.batch_size,
+        if is_train:
+            # create a shared epoch store to sync epoch to dataloader worker proc
+            shared_interval_count = SharedCount(count=start_interval)
+        else:
+            shared_interval_count = None
+        num_batches = len(dataset) // cfg.batch_size
+
+        base_loader = DataLoader(
+            dataset=dataset, 
+            collate_fn=collate_fn,
+            sampler=training_sampler, 
+            batch_size=cfg.batch_size, 
             num_workers=cfg.num_workers,
-            sampler=training_sampler,
-        )
+            )
+        loader = LoaderBundle(
+        loader=base_loader,
+        num_batches=num_batches,
+        num_samples=cfg.num_samples,
+        shared_interval=shared_interval_count,
+    )
     return loader
