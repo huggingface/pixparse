b9578eb67c6acb14dc1533a905e720033ce2f854
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2023-08-22 16:43:43 +0200

    add rvlcdip prediction

--- src/pixparse/task/__init__.py
+++ src/pixparse/task/__init__.py
@@ -1,5 +1,10 @@
 from .task_cruller_pretrain import TaskCrullerPretrain, TaskCrullerPretrainCfg
-from .task_cruller_finetune import TaskCrullerFinetune, TaskCrullerFinetuneCfg
+from .task_cruller_finetune_RVLCDIP import TaskCrullerFinetuneRVLCDIP, TaskCrullerFinetuneRVLCDIPCfg
+from .task_cruller_finetune_xent import (
+    TaskCrullerFinetuneXent,
+    TaskCrullerFinetuneXentCfg,
+)
+
 from .task_cruller_eval_ocr import TaskCrullerEvalOCR, TaskCrullerEvalOCRCfg
 from .task_donut_eval_ocr import TaskDonutEvalOCR, TaskDonutEvalOCRCfg
 from .task_factory import TaskFactory
