b480a3df451a5c64350f15a23204edb94dcabb66
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2023-09-05 11:05:13 +0000

    add custom docVQA wrapper

--- src/pixparse/data/datasets_utils.py
+++ src/pixparse/data/datasets_utils.py
@@ -1,8 +1,11 @@
+import json
+import os
 from ast import literal_eval
 
 import torch
 from datasets import load_dataset
-from torch.utils.data import DataLoader
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 
 from pixparse.utils.json_utils import json2token
@@ -17,6 +20,43 @@ In particular for json processing of datasets, each key becomes a
 new special token, and the tokenizer vocab needs to be resized on the fly. 
 """
 
+class CustomVQADataset(Dataset):
+    """
+    Custom implementation of the SinglePageDocVQA dataset.
+    len(train_images), len(test_images), len(val_images)
+    is (10194, 1287, 1286). Each image has one or more questions and answers attached, or solely questions for the test set.
+    """
+    def __init__(self, root_dir, split, transform=None):
+        self.extra_tokens = ['<s_answer>', '</s_answer>', '</s_question>', '<s_question>']
+        self.root_dir = root_dir
+        self.split = split
+        assert split in ["train", "test", "val"], "split is not train, test or val."
+        json_path = os.path.join(root_dir, split, f"processed_{split}_v1.0.json")
+        assert os.path.isdir(self.root_dir), f"Can't find {root_dir}. Make sure you have DocVQA files locally."
+        assert os.path.isfile(json_path), f"{json_path} not found. Make sure you have the processed dataset."
+        self.img_dir = os.path.join(root_dir, split)
+        
+        with open(json_path, 'r') as f:
+            self.data_dict = json.load(f)
+        self.all_images = list(self.data_dict.keys())
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.all_images)
+    
+    def __getitem__(self, index):
+        image_id = self.all_images[index]
+        questions_and_answers = self.data_dict[image_id]
+        # For the test split, there is no answer.
+
+        labels = questions_and_answers
+
+        img_path = os.path.join(self.img_dir, image_id)
+        image = Image.open(img_path).convert("L")
+        if self.transform:
+            image = self.transform(image)
+        
+        return {"image": image, "labels": labels}
 
 class SafeDataset:
     """
