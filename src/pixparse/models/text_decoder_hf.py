6797577e538c64704214f389811f3c07822a45f6
Ross Wightman <rwightman@gmail.com>
2023-10-04 16:59:05 -0700

    Errant line made it into checkpointing commit

--- src/pixparse/models/text_decoder_hf.py
+++ src/pixparse/models/text_decoder_hf.py
@@ -13,7 +13,6 @@ def _hf_text_decoder(cfg: TextDecoderCfg) -> transformers.BartForCausalLM:  # FI
     config = transformers.AutoConfig.from_pretrained(cfg.name)
     config.is_decoder = True
     config.add_cross_attention = True
-    transformers.BartForCausalLM
     if False:  # FIXME these were set in Donut but missed in first pass, should compare
         config.is_encoder_decoder = False
         config.scale_embedding = True
